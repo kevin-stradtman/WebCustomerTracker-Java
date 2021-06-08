f6c25b152b9aeee1d0f9074f45951c75f6a68aeb
Kevin Stradtman <kevin.stradtman@gmail.com>
2021-06-08 17:41:23 -0400

    Added CustomerDAO and the CustomerDAOImpl class

--- src/com/stradtman/springdemo/controller/CustomerController.java
+++ src/com/stradtman/springdemo/controller/CustomerController.java
@@ -1,14 +1,26 @@
 package com.stradtman.springdemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.stradtman.springdemo.dao.CustomerDAO;
+import com.stradtman.springdemo.entity.Customer;
+
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+	
+	@Autowired
+	private CustomerDAO customerDAO;
+	
 	@RequestMapping("/list")
 	public String listCustomers(Model theModel) {
+		List<Customer> theCustomers = customerDAO.getCustomers();
+		theModel.addAttribute("customers", theCustomers);
 		return "list-customers";
 	}
 }
--- src/com/stradtman/springdemo/dao/CustomerDAO.java
+++ src/com/stradtman/springdemo/dao/CustomerDAO.java
@@ -1,5 +1,11 @@
 package com.stradtman.springdemo.dao;
 
+import java.util.List;
+
+import com.stradtman.springdemo.entity.Customer;
+
 public interface CustomerDAO {
 
+	public List<Customer> getCustomers();
+	
 }
